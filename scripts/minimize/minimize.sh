9f0b459d906aa1f4c5233913dd902d3be61d7261
Marcos Augusto <marcosaugustoldo@proton.me>
2026-04-22 22:25:27 -0300

    Delete scripts/minimize directory

--- scripts/minimize/minimize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Move a janela ativa para o workspace especial SEM levar o foco junto
-hyprctl dispatch movetoworkspacesilent special:minimized
-
-# Atualiza APENAS o contador de minimizadas (Signal 11)
-pkill -RTMIN+11 waybar
